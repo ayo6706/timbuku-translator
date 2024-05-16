1b6ba645698e07d376c158beacc8f84dc23a5a2e
Ayomide Onibokun <ayo6706@gmail.com>
2024-05-16 01:29:49 +0100

    pie in

---
+++ pages/api/upload.js
@@ -0,0 +1,28 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
+import { processImage, generateText } from '../../lib/gcp'; 
+
+const upload = multer({ dest: 'uploads/' });
+
+const handler = nextConnect();
+
+handler.use(upload.single('image'));
+
+handler.post(async (req, res) => {
+    const imageFile = req.file.path;
+    try {
+        const imageText = await processImage(imageFile);
+        const response = await generateText(imageText);
+        res.status(200).json({ response });
+    } catch (error) {
+        // console.error('Error in handler.post:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+export default handler;
